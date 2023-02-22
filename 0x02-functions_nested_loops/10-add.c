cf57088f44d299eeb3c974cda30ad2234bc73400
codewithKayyy <mr.kayy2003@gmail.com>
2023-02-22 10:05:51 -0800

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * int - adds two numbers
+ *
+ * @a: parameter 1
+ *
+ * @b: parameter 2
+ *
+ * Return: sum of a and b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
